0645813040899508d3b11681fc4e162fdbb88849
tadaren <ptymaato@yahoo.co.jp>
2021-03-26 14:18:32 +0900

    ヘッダーにあるブックマークリストへのリンクの見た目を修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { FaStar } from 'react-icons/fa';
 import { SearchBox } from './SearchBox';
 
 export const Header: FC = () => {
@@ -19,10 +20,9 @@ export const Header: FC = () => {
                     </a>
                 </Link>
                 <Link href="/bookmarks">
-                    <a className="text-center border p-1 rounded-lg bg-gray-100 my-2 mr-2 hover:bg-gray-200">
-                        お気に入り
-                        <br />
-                        レシピ
+                    <a className="text-center border p-1 rounded-lg bg-gray-100 my-2 mr-2 hover:bg-gray-200 flex items-center">
+                        <FaStar color="#FCD34D" />
+                        一覧
                     </a>
                 </Link>
             </div>
